33a19418e650a81bdc8b49da80080a1f90b6d5dd
automaar <marcos.samudio@alumnos.colegiokronos.cl>
2023-05-25 18:48:53 -0400

    Final

--- react_app/src/Pages/Profile/Profile.jsx
+++ react_app/src/Pages/Profile/Profile.jsx
@@ -12,18 +12,18 @@ export default function Profile(){
 
 
             <div className="flex items-center justify-center py-5">
-                <div className="flex flex-cols-2 justify-center text-center bg-slate-300 bg-opacity-50 rounded-lg shadow-2xl w-3/5 p-12">
+                <div className="flex flex-cols-2 justify-items-center text-center bg-slate-300 bg-opacity-50 rounded-lg shadow-2xl w-3/5 h-96 p-12 pt-28">
 
                     <div className="pr-5">
                         <img src={loggedUser.image} alt="ImagenProfile" className="w-36 h-36 rounded-full object-cover"/>
                     </div>
 
-                    <div className="border-l border-indigo-500">
-                        <h1 className="border-b border-indigo-500 flex p-2">Nombre:<p className="pl-2">  {loggedUser.alias}</p></h1>
+                    <div className="">
+                        <h1 className="border-b border-l border-indigo-500 flex p-2">Nombre:<p className="pl-2">  {loggedUser.alias}</p></h1>
 
-                        <h1 className="border-b border-indigo-500 flex p-2">Correo: <p className="pl-2">{loggedUser.email}</p></h1>
+                        <h1 className="border-b border-l border-indigo-500 flex p-2">Correo: <p className="pl-2">{loggedUser.email}</p></h1>
 
-                        <h1 className="flex p-2">Contraseña: <p className="pl-2">{loggedUser.password}</p> </h1>
+                        <h1 className="border-l border-indigo-500 flex p-2">Contraseña: <p className="pl-2">{loggedUser.password}</p> </h1>
                     </div>
                     
                 </div>
